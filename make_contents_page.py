11ca7d14bacccb114a72d5a227c5e1871d8113df
tomstafford <tom@idiolect.org.uk>
2018-08-21 10:27:12 +0100

    contents as md

--- make_contents_page.py
+++ make_contents_page.py
@@ -7,16 +7,16 @@ import glob
 
 
 def make_link(filename):
-    return '<a href="'+filename.split('.')[0]+'">'+filename.split('.')[0]+'</a>'
+    return '['+filename.split('.')[0]+']('+filename.split('.')[0]+')'
 
 def pad(item):
-    return '<br>\n'+item+'\n<br>\n'
+    return '\n'+item+'\n'
 
 filenames=sorted(glob.glob('*.md'))
 
 
 
-with open('contents.html', 'w') as outfile:
+with open('contents.md', 'w') as outfile:
 
     for filename in filenames:
         outfile.write(pad(make_link(filename)))
